0e3b4f1690058d46992b750d9af0592ef90ed2a9
unknown <sobakoved@inbox.lv>
2023-02-26 04:04:45 +0000

    Adds array for team members

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+const teamMembers = [];
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
@@ -43,28 +44,83 @@ inquirer.prompt([
 
     manager = new Manager(r.userName,r.userID,r.userEmail,r.userNumber)
 console.log(manager)
+teamMembers.push(manager);
     // populate manager info
-    // promptForNexEMployee ()
+     promptForNextEmployee ();
 })
 
 const promptForNextEmployee = () => {
-    inquirer.prompt([{
-        // choice of 3
-    }]).then(response => {
-        // if engineer
-        //    promptForEngineer
-        // else if intern
-        //    promptForIntern
-        // else
-        //    use the functionality from page-template to generate the team
+    inquirer.prompt([
+
+        {
+            type: 'checkbox',
+            name: 'nextEmployee',
+            message: 'Please add new employee:',
+            choices: [
+              'Add an engineer', 'Add an intern', 'Finish building the team', 
+            ],
+            
+          },
+
+
+
+       
+    ]).then(response => {
+
+        // get user selection
+        let newEmployee = response.nextEmployee[0];
+        console.log(newEmployee)
+        // process relevant choice
+        switch(newEmployee) {
+            case "Add an engineer":
+                promptForEngineer();
+               // console.log("Engineer")
+              break;
+            case "Add an intern":
+                //    promptForIntern
+                console.log("intern")
+              break;
+             
+            default:
+               //    use the functionality from page-template to generate the team
+               console.log("finish")
+          } 
+        
     })
 }
 
 const promptForEngineer = () => {
-    inquirer.prompt([{
+    inquirer.prompt([
+
+        {
+            type: 'input',
+            message: 'Please enter engineer name:',
+            name: 'userName',
+        },
+        {
+            type: 'input',
+            message: 'Please enter employee ID:',
+            name: 'userID',
+        },
+        {
+            type: 'input',
+            message: 'Please enter employee email:',
+            name: 'userEmail',
+        },
+        {
+            type: 'input',
+            message: 'Please enter employee GitHub username:',
+            name: 'userGitHub',
+        },
+
+
         //engineer questions
-    }]).then(response => {
+    ]).then(r => {
         // add new engineer to employees array
+        const engineer = new Engineer(r.userName,r.userID,r.userEmail,r.userGitHub)
+        console.log(engineer)
+        teamMembers.push(engineer);
+        console.log(teamMembers)
         // promptForNextEmployee
     })
 }
