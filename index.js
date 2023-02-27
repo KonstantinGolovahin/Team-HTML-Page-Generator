8d321777ea889ea3cbc36cfeac042089fa86efd4
unknown <sobakoved@inbox.lv>
2023-02-27 03:27:31 +0000

    Adds styling with style.css

--- index.js
+++ index.js
@@ -15,156 +15,147 @@ const render = require("./src/page-template.js");
 // array for storing team members
 const teamMembers = [];
 
+// Thanks to https://stackoverflow.com/questions/48944397/repeat-set-of-questions-in-inquirerjs
+const questionId = {
+    type: 'input',
+    message: 'Please enter employee ID:',
+    name: 'userID',
+}
+
+const questionEmail = {
+    type: 'input',
+    message: 'Please enter employee email:',
+    name: 'userEmail',
+}
+
+const questionName = {
+
+    type: 'input',
+    message: 'Please enter employee name:',
+    name: 'userName',
+
+}
+
+const questionManager = {
+    type: 'input',
+    message: 'Please enter team manager name:',
+    name: 'userName',
+}
+
+const questionOffice = {
+
+    type: 'input',
+    message: 'Please enter employee office number:',
+    name: 'userNumber',
+}
+const questionGithub = {
+    type: 'input',
+    message: 'Please enter employee GitHub username:',
+    name: 'userGitHub',
+}
+const questionSchool = {
+    type: 'input',
+    message: 'Please enter employee school:',
+    name: 'userSchool',
+}
+
+const questionSelection = {
+    type: 'checkbox',
+    name: 'nextEmployee',
+    message: 'Please add new employee:',
+    choices: [
+        'Add an engineer', 'Add an intern', 'Finish building the team',
+    ],
+
+}
+
 // start with getting a manager
 inquirer.prompt([
 
-    {
-        type: 'input',
-        message: 'Please enter team manager name:',
-        name: 'userName',
-    },
-    {
-        type: 'input',
-        message: 'Please enter employee ID:',
-        name: 'userID',
-    },
-    {
-        type: 'input',
-        message: 'Please enter employee email:',
-        name: 'userEmail',
-    },
-    {
-        type: 'input',
-        message: 'Please enter employee office number:',
-        name: 'userNumber',
-    },
-    
-]).then(r => {
-// add new manager
-    manager = new Manager(r.userName,r.userID,r.userEmail,r.userNumber)
+    questionManager,
+    questionId,
+    questionEmail,
+    questionOffice,
 
-teamMembers.push(manager);
-   
-     promptForNextEmployee ();
+]).then(r => {
+    // add new manager
+    manager = new Manager(r.userName, r.userID, r.userEmail, r.userNumber)
+
+    teamMembers.push(manager);
+
+    promptForNextEmployee();
 })
 
 const promptForNextEmployee = () => {
     inquirer.prompt([
 
-        {
-            type: 'checkbox',
-            name: 'nextEmployee',
-            message: 'Please add new employee:',
-            choices: [
-              'Add an engineer', 'Add an intern', 'Finish building the team', 
-            ],
-            
-          },
-       
+        questionSelection,
+
     ]).then(response => {
 
         // get user selection
         let newEmployee = response.nextEmployee[0];
         console.log(newEmployee)
         // process relevant choice
-        switch(newEmployee) {
+        switch (newEmployee) {
             case "Add an engineer":
                 promptForEngineer();
-             
-              break;
+
+                break;
             case "Add an intern":
                 promptForIntern();
-            
-              break;
-             
+
+                break;
+
             default:
-              // if no new members added              
-               buildPage();
-          } 
-        
+                // if no new members added              
+                buildPage();
+        }
+
     })
 }
 
 const promptForEngineer = () => {
     inquirer.prompt([
 
-        {
-            type: 'input',
-            message: 'Please enter employee name:',
-            name: 'userName',
-        },
-        {
-            type: 'input',
-            message: 'Please enter employee ID:',
-            name: 'userID',
-        },
-        {
-            type: 'input',
-            message: 'Please enter employee email:',
-            name: 'userEmail',
-        },
-        {
-            type: 'input',
-            message: 'Please enter employee GitHub username:',
-            name: 'userGitHub',
-        },
+        questionName,
+        questionId,
+        questionEmail,
+        questionGithub,
 
-
-        
     ]).then(r => {
         // add new engineer to employees array
-        const engineer = new Engineer(r.userName,r.userID,r.userEmail,r.userGitHub)
-       
+        const engineer = new Engineer(r.userName, r.userID, r.userEmail, r.userGitHub)
         teamMembers.push(engineer);
-        
-        promptForNextEmployee ();
-       
+        promptForNextEmployee();
+
     })
 }
 
 const promptForIntern = () => {
     inquirer.prompt([
-
-        {
-            type: 'input',
-            message: 'Please enter employee name:',
-            name: 'userName',
-        },
-        {
-            type: 'input',
-            message: 'Please enter employee ID:',
-            name: 'userID',
-        },
-        {
-            type: 'input',
-            message: 'Please enter employee email:',
-            name: 'userEmail',
-        },
-        {
-            type: 'input',
-            message: 'Please enter employee school:',
-            name: 'userSchool',
-        },
         //intern questions
+        questionName,
+        questionId,
+        questionEmail,
+        questionSchool,
+
     ]).then(r => {
         // new intern
-        const intern = new Intern(r.userName,r.userID,r.userEmail,r.userSchool)
-       
+        const intern = new Intern(r.userName, r.userID, r.userEmail, r.userSchool)
         teamMembers.push(intern);
-       // ask for new team member
-        promptForNextEmployee ();
+        // ask for new team member
+        promptForNextEmployee();
 
     })
 }
 
 const buildPage = () => {
- 
-// generate actual HTML page 
- fs.writeFile(outputPath, render(teamMembers), function (err) {
-    if (err) throw err;
-    console.log('File Saved!');
-  });
-   
 
+    // generate actual HTML page 
+    fs.writeFile(outputPath, render(teamMembers), function (err) {
+        if (err) throw err;
+        console.log('File Saved!');
+    });
 }
 
